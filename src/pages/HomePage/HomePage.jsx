7f284477c161f565217bcdceaeac6ccbf307742c
olita16 <119816415+olita16@users.noreply.github.com>
2024-11-03 22:25:58 +0100

    check way to images

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,7 +4,7 @@ import css from "./HomePage.module.css"
 const HomePage = () => {
   return (
     <div>
-      <img className={css.imageBg} src="/public/Phone-bg.jpg" width={500} alt="Managing contacts" />
+      <img className={css.imageBg} src="/public/images/Phone-bg.jpg" width={500} alt="Managing contacts" />
     </div>
   );
 };
